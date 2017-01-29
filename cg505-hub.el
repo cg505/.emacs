317fc6b60f7e738eb4ecb51727a46d3e07b48a93
Christopher Cooper <christopher.c.cooper@gmail.com>
2017-01-29 13:14:30 -0600

    add windmove etc keybinds

--- cg505-hub.el
+++ cg505-hub.el
@@ -1,3 +1,13 @@
-(message "YOU DID IT2")
+;; windmove keybindings with shift
+(global-set-key (kbd "C-S-i") 'windmove-up)
+(global-set-key (kbd "C-S-j") 'windmove-left)
+(global-set-key (kbd "C-S-k") 'windmove-down)
+(global-set-key (kbd "C-S-l") 'windmove-right)
+(global-set-key (kbd "C-M-S-i") 'buf-move-up)
+(global-set-key (kbd "C-M-S-j") 'buf-move-left)
+(global-set-key (kbd "C-M-S-k") 'buf-move-down)
+(global-set-key (kbd "C-M-S-l") 'buf-move-right)
+
+
 
 (provide 'cg505-hub)
